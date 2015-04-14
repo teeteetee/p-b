1f1e09d706828a62363d0c6ae364255391b0219c
teeteetee <proto@protos-MacBook-Pro.local>
2015-04-14 15:36:47 -0700

    --

--- app.js
+++ app.js
@@ -161,6 +161,7 @@ app.post('/addbook',function(req,res){
       vauthor = '--';
     }
     users.update({uid:vuid},{$push:{books:{author:vauthor,booktitle:vbooktitle,newbook:vnewbook,star:vstar,attention:vattention,regdateint:fulldate}}});
+    users.update({uid:vuid},{$inc:{newbooks:1}});
     ms.trouble=0;
     res.send(ms);
   }
