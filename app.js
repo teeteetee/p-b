7641b2c6523f06baf05cb5a1d29b28825469a825
teeteetee <proto@protos-MacBook-Pro.local>
2015-04-20 04:02:01 -0700

    --

--- app.js
+++ app.js
@@ -150,19 +150,8 @@ app.get('/lists/:lid',function(req,res){
                       if(!moviesvar) {
                         moviesvar =0;
                       }
-                      var style='';
-                      var style1 = "'mh':'#FAB142','bh':'#52CA8F','mhl':'#FAC26C','bhl':'rgb(123, 226, 175)',";
-                      var style2 = "'mh':'rgb(213, 74, 95)','bh':'#00788E','mhl':'#F56B80','bhl':'rgb(0, 176, 208)',";
-                      var style3 = "'mh':'#00657C','bh':'rgb(0, 182, 205);','mhl':'#003945','bhl':'rgb(82, 214, 231)',";
-                      if(done.styleint===1){
-                        style=style1;
-                      }
-                      if(done.styleint===2){
-                        style=style2;
-                      }
-                      if(done.styleint===3){
-                        style=style3;
-                      }
+                    
+                      var style = "'mh':'white','bh':'white','mhl':'#F56979','bhl':'rgb(103, 186, 255),'small':'','spanop':'0.1',";
                     eval("res.render('listin',{"+style+"'mail':done.mail,'books':booksvar,'movies':moviesvar,'uid':done.uid,'newbooks':done.newbooks,'readbooks':done.readbooks,'newmovies':done.newmovies,'seenmovies':done.seenmovies});");
            
            }
