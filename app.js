acc003a19919531b439130ae59dc49dae247c893
teeteetee <proto@protos-MacBook-Pro.local>
2015-04-20 04:04:50 -0700

    --

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ app.get('/lists/:lid',function(req,res){
                         moviesvar =0;
                       }
                     
-                      var style = "'mh':'white','bh':'white','mhl':'#F56979','bhl':'rgb(103, 186, 255),'small':'','spanop':'0.1',";
+                      var style = "'mh':'white','bh':'white','mhl':'#F56979','bhl':'rgb(103, 186, 255),'small':'0:0','spanop':'0.1',";
                       console.log(style);
                     console.log("res.render('listin',{"+style+"'mail':done.mail,'books':booksvar,'movies':moviesvar,'uid':done.uid,'newbooks':done.newbooks,'readbooks':done.readbooks,'newmovies':done.newmovies,'seenmovies':done.seenmovies});");
 
